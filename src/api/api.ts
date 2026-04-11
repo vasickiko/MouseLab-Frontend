cd29847133d48c5714eadf5ef6be895143132efd
Kristijan Vasic <157734449+vasickiko@users.noreply.github.com>
2026-04-11 11:08:52 +0200

    URL Updated

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://mouselab-frontend.netlify.app/",
 })
 
 api.interceptors.request.use((config) => {
